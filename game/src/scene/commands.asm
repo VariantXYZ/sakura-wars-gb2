10dd885fc96d19a00f1ae902a5924e0e47bf5df2
VariantXYZ <VariantXYZ@users.noreply.github.com>
2024-11-02 22:49:40 -0700

    Guess parameter sizes for now to dump probable text commands

---
+++ game/src/scene/commands.asm
@@ -0,0 +1,252 @@
+MACRO Unknown00
+  db $00
+  IF _NARG > 0
+    db \#
+  ENDC
+ENDM
+
+MACRO Unknown01
+  db $01
+  IF _NARG > 0
+    db \#
+  ENDC
+ENDM
+
+MACRO Unknown02
+  db $02
+  IF _NARG > 0
+    db \#
+  ENDC
+ENDM
+
+MACRO Unknown03
+  db $03
+  IF _NARG > 0
+    db \#
+  ENDC
+ENDM
+
+MACRO Unknown04
+  db $04
+  IF _NARG > 0
+    db \#
+  ENDC
+ENDM
+
+MACRO Unknown05
+  db $05
+  IF _NARG > 0
+    db \#
+  ENDC
+ENDM
+
+MACRO Unknown06
+  db $06
+  IF _NARG > 0
+    db \#
+  ENDC
+ENDM
+
+MACRO Unknown07
+  db $07
+  IF _NARG > 0
+    db \#
+  ENDC
+ENDM
+
+MACRO Unknown08
+  db $08
+  IF _NARG > 0
+    db \#
+  ENDC
+ENDM
+
+MACRO Unknown09
+  db $09
+  IF _NARG > 0
+    db \#
+  ENDC
+ENDM
+
+MACRO Unknown0A
+  db $0A
+  IF _NARG > 0
+    db \#
+  ENDC
+ENDM
+
+MACRO Unknown0B
+  db $0B
+  IF _NARG > 0
+    db \#
+  ENDC
+ENDM
+
+MACRO SetPosition
+  db $0C
+  IF _NARG > 0
+    db \#
+  ENDC
+ENDM
+
+MACRO TextCommand
+  db $0D
+  IF _NARG > 0
+    db \#
+  ENDC
+ENDM
+
+MACRO Unknown0E
+  db $0E
+  IF _NARG > 0
+    db \#
+  ENDC
+ENDM
+
+MACRO Unknown0F
+  db $0F
+  IF _NARG > 0
+    db \#
+  ENDC
+ENDM
+
+MACRO Unknown10
+  db $10
+  IF _NARG > 0
+    db \#
+  ENDC
+ENDM
+
+MACRO Unknown11
+  db $11
+  IF _NARG > 0
+    db \#
+  ENDC
+ENDM
+
+MACRO Unknown12
+  db $12
+  IF _NARG > 0
+    db \#
+  ENDC
+ENDM
+
+MACRO Unknown13
+  db $13
+  IF _NARG > 0
+    db \#
+  ENDC
+ENDM
+
+MACRO Unknown14
+  db $14
+  IF _NARG > 0
+    db \#
+  ENDC
+ENDM
+
+MACRO Unknown15
+  db $15
+  IF _NARG > 0
+    db \#
+  ENDC
+ENDM
+
+MACRO WaitInputA
+  db $16
+  IF _NARG > 0
+    db \#
+  ENDC
+ENDM
+
+MACRO WaitInputDownFaceUp
+  db $17
+  IF _NARG > 0
+    db \#
+  ENDC
+ENDM
+
+MACRO WaitInputLeft
+  db $18
+  IF _NARG > 0
+    db \#
+  ENDC
+ENDM
+
+MACRO WaitInputRight
+  db $19
+  IF _NARG > 0
+    db \#
+  ENDC
+ENDM
+
+MACRO WaitInputUp
+  db $1A
+  IF _NARG > 0
+    db \#
+  ENDC
+ENDM
+
+MACRO WaitInputDown
+  db $1B
+  IF _NARG > 0
+    db \#
+  ENDC
+ENDM
+
+MACRO WaitInputLeft2
+  db $1C
+  IF _NARG > 0
+    db \#
+  ENDC
+ENDM
+
+MACRO WaitInputAorUp
+  db $1E
+  IF _NARG > 0
+    db \#
+  ENDC
+ENDM
+
+MACRO Unknown1F
+  db $1F
+  IF _NARG > 0
+    db \#
+  ENDC
+ENDM
+
+MACRO Unknown20
+  db $20
+  IF _NARG > 0
+    db \#
+  ENDC
+ENDM
+
+MACRO Unknown21
+  db $21
+  IF _NARG > 0
+    db \#
+  ENDC
+ENDM
+
+MACRO Unknown23
+  db $23
+  IF _NARG > 0
+    db \#
+  ENDC
+ENDM
+
+MACRO Unknown24
+  db $24
+  IF _NARG > 0
+    db \#
+  ENDC
+ENDM
+
+MACRO Unknown25
+  db $25
+  IF _NARG > 0
+    db \#
+  ENDC
+ENDM
+
--- scripts/sw2gb/game.py
+++ scripts/sw2gb/game.py
@@ -51,9 +51,43 @@ class GameSceneScript:
         self.character_table_reversed = { character_table[key]: key for key in character_table }
         self.character_widths_table = character_widths_table
 
-
         self.COMMANDS = AliasDict()
+        self.COMMANDS[0x00] = GameSceneScript.CommandFunction.dummy("Unknown00", 0x00, 0)
+        self.COMMANDS[0x01] = GameSceneScript.CommandFunction.dummy("Unknown01", 0x01, 3)
+        self.COMMANDS[0x02] = GameSceneScript.CommandFunction.dummy("Unknown02", 0x02, 0)
+        self.COMMANDS[0x03] = GameSceneScript.CommandFunction.dummy("Unknown03", 0x03, 2)
+        self.COMMANDS[0x04] = GameSceneScript.CommandFunction.dummy("Unknown04", 0x04, 0)
+        self.COMMANDS[0x05] = GameSceneScript.CommandFunction.dummy("Unknown05", 0x05, 4)
+        self.COMMANDS[0x06] = GameSceneScript.CommandFunction.dummy("Unknown06", 0x06, 2)
+        self.COMMANDS[0x07] = GameSceneScript.CommandFunction.dummy("Unknown07", 0x07, 1)
+        self.COMMANDS[0x08] = GameSceneScript.CommandFunction.dummy("Unknown08", 0x08, 0)
+        self.COMMANDS[0x09] = GameSceneScript.CommandFunction.dummy("Unknown09", 0x09, 0)
+        self.COMMANDS[0x0A] = GameSceneScript.CommandFunction.dummy("Unknown0A", 0x0A, 3)
+        self.COMMANDS[0x0B] = GameSceneScript.CommandFunction.dummy("Unknown0B", 0x0B, 3)
+        self.COMMANDS[0x0C] = GameSceneScript.CommandFunction.dummy("SetPosition", 0x0C, 3) # 0C <X> <Y> <Scene ID>
+        self.COMMANDS[0x0D] = GameSceneScript.CommandFunction.dummy("TextCommand", 0x0D, 3)
+        self.COMMANDS[0x0E] = GameSceneScript.CommandFunction.dummy("Unknown0E", 0x0E, 4)
+        self.COMMANDS[0x0F] = GameSceneScript.CommandFunction.dummy("Unknown0F", 0x0F, 1)
+        self.COMMANDS[0x10] = GameSceneScript.CommandFunction.dummy("Unknown10", 0x10, 4)
+        self.COMMANDS[0x11] = GameSceneScript.CommandFunction.dummy("Unknown11", 0x11, 0)
+        self.COMMANDS[0x12] = GameSceneScript.CommandFunction.dummy("Unknown12", 0x12, 0)
+        self.COMMANDS[0x13] = GameSceneScript.CommandFunction.dummy("Unknown13", 0x13, 0)
+        self.COMMANDS[0x14] = GameSceneScript.CommandFunction.dummy("Unknown14", 0x14, 0)
+        self.COMMANDS[0x15] = GameSceneScript.CommandFunction.dummy("Unknown15", 0x15, 0)
+        self.COMMANDS[0x16] = GameSceneScript.CommandFunction.dummy("WaitInputA", 0x16, 0)
+        self.COMMANDS[0x17] = GameSceneScript.CommandFunction.dummy("WaitInputDownFaceUp", 0x17, 0)
+        self.COMMANDS[0x18] = GameSceneScript.CommandFunction.dummy("WaitInputLeft", 0x18, 0)
+        self.COMMANDS[0x19] = GameSceneScript.CommandFunction.dummy("WaitInputRight", 0x19, 0)
+        self.COMMANDS[0x1A] = GameSceneScript.CommandFunction.dummy("WaitInputUp", 0x1A, 0)
+        self.COMMANDS[0x1B] = GameSceneScript.CommandFunction.dummy("WaitInputDown", 0x1B, 0)
+        self.COMMANDS[0x1C] = GameSceneScript.CommandFunction.dummy("WaitInputLeft2", 0x1C, 0)
+        self.COMMANDS[0x1E] = GameSceneScript.CommandFunction.dummy("WaitInputAorUp", 0x1E, 0)
+        self.COMMANDS[0x1F] = GameSceneScript.CommandFunction.dummy("Unknown1F", 0x1F, 2)
         self.COMMANDS[0x20] = GameSceneScript.CommandFunction.dummy("Unknown20", 0x20, 2)
+        self.COMMANDS[0x21] = GameSceneScript.CommandFunction.dummy("Unknown21", 0x21, 2)
+        self.COMMANDS[0x23] = GameSceneScript.CommandFunction.dummy("Unknown23", 0x23, 2)
+        self.COMMANDS[0x24] = GameSceneScript.CommandFunction.dummy("Unknown24", 0x24, 2)
+        self.COMMANDS[0x25] = GameSceneScript.CommandFunction.dummy("Unknown25", 0x25, 2)
 
         # Create aliases so we can look up by name and command byte
         for key in self.COMMANDS:
